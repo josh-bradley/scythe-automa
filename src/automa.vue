30eb226a7eeec3806893fbed169e39bdc2099158
Josh <josh.bradley@readify.net>
2018-01-14 16:24:31 +1100

    hooking up the star position to the redux store

--- src/automa.vue
+++ src/automa.vue
@@ -14,17 +14,24 @@
   import png from './imageImport.js'
   import data from './assets/data.js'
   import { createStore } from 'redux'
+  import reducer from './redux/reducers/index'
+  import { dealCard, addPlayer } from './redux/actions/index'
+
+  const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  store.dispatch(addPlayer(1))
 
   function getRandomInt() {
     var min = 1;
-    var max = 19;
+    var max = 20;
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
   }
 
   var dealtCards = [];
 
   export default {  
-    data () { return {
+    data () {
+      return {
         image: './dist/20.png',
         starCard:'./dist/StarCard0.png',
         starCount:0,
@@ -32,25 +39,26 @@
         starCardPos:0
       }
     },
+    created: function(){
+      let handleChange = (() => {
+        const state = store.getState();
+        this.starCardPos = state.players[0].starCardPosition;
+      }).bind(this)
+      store.subscribe(handleChange)
+    },
     methods: {
       dealNextCard: function(){
         var nextCardNumber = 0;
-        if(dealtCards.length === 19) {
-          nextCardNumber = 20;
-          dealtCards = [];
-        }
-
+        const player = store.getState().players[0];
+        console.log('dealing cards')
         while(nextCardNumber === 0) {
           var cardNumber = getRandomInt();
-          if(!dealtCards.some(x => { x === cardNumber})){
-            dealtCards.push(nextCardNumber);
+          if(player.dealtCards.length >= 19 || !player.dealtCards.some(x => x === cardNumber)){
             nextCardNumber = cardNumber;
           }
         }
+        store.dispatch(dealCard(0, nextCardNumber));
         var cardData = data.cards[nextCardNumber];
-        if(cardData && cardData.star) {
-          this.starCardPos++;
-        }
         this.image = `./dist/${nextCardNumber.toString().padStart(2, '0')}.png`
       }
     }
