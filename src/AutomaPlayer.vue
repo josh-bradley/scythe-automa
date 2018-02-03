5a4c9f8a9c6f6457f13afcb5ea536463713e253f
Josh <josh.bradley@readify.net>
2018-02-03 20:40:10 +1100

    moved the card drawing functionality to outside of the player component

--- src/AutomaPlayer.vue
+++ src/AutomaPlayer.vue
@@ -1,30 +1,17 @@
 <template lang='pug'>
   div
-    img(width='200' :src='image' @click='dealNextCard')
-    img(width='200' :src='starCard' @click='dealNextCard')
-    div
-      | Current game turn: {{currentTurn}}
+    img(width='200' :src='image')
+    img(width='200' :src='starCard')
     div
       | Star card position: {{starCardPos}}
     div
       | Star count: {{starCount}}
-    div
-      | {{image}}
 </template>
 
 <script>
-  import data from './assets/data.js'
-  import * as types from './vuex/types'
-  function getRandomInt() {
-      var min = 1;
-      var max = 20;
-      return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-  }
-
   export default {
     data () {
       return {
-        image: './dist/20.png',
         starCard:'./dist/StarCard0.png',
         starCount:0,  
         starCardId:0,
@@ -42,22 +29,11 @@
       },
       currentTurn () {
         return this.$store.state.currentTurn;
-      }
-    },
-    methods: {
-      dealNextCard: function(){
-        var nextCardNumber = 0;
-        const player = this.$store.state.players[this.playerId];
-        console.log('dealing cards')
-        while(nextCardNumber === 0) {
-          var cardNumber = getRandomInt();
-          if(player.dealtCards.length >= 19 || !player.dealtCards.some(x => x === cardNumber)){
-            nextCardNumber = cardNumber;
-          }
-        }
-        this.$store.commit(types.DEAL_CARD, { card:nextCardNumber});
-        var cardData = data.cards[nextCardNumber];
-        this.image = `./dist/${nextCardNumber.toString().padStart(2, '0')}.png`
+      },
+      image () {
+        let dealtCards = this.$store.state.players[this.playerId].dealtCards;
+        let nextCardNumber = dealtCards.length > 0 ? dealtCards[dealtCards.length - 1] : '20';
+        return `./dist/${nextCardNumber.toString().padStart(2, '0')}.png`
       }
     },
   }
--- src/automa.vue
+++ src/automa.vue
@@ -3,14 +3,25 @@
     | {{currentTurn}}
     div(v-for="(player, index) in players") 
       AutomaPlayer(:playerId="index")
+    div
+      | Current turn {{currentTurn}}
+    button(@click='dealNextCard')
+      | Deal Card
 </template>
 
 <script>
   import png from './imageImport.js'
   import storeVuex from './vuex/store'
+  import data from './assets/data.js'
   import * as types from './vuex/types'
   import AutomaPlayer from './AutomaPlayer.vue'
   import { mapState } from 'vuex'
+
+  function getRandomInt() {
+      var min = 1;
+      var max = 20;
+      return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
 
   storeVuex.commit(types.ADD_PLAYER, { level:1 });
   storeVuex.commit(types.ADD_PLAYER, { level:1 });
@@ -25,6 +36,22 @@
     computed: mapState([
       'players',
       'currentTurn'
-    ])
+    ]),
+    methods: {
+      dealNextCard: function(){
+        var nextCardNumber = 0;
+        let nextPlayerIndex = this.$store.state.currentTurn % this.$store.state.players.length;
+        const player = this.$store.state.players[nextPlayerIndex];
+        while(nextCardNumber === 0) {
+          var cardNumber = getRandomInt();
+          if(player.dealtCards.length >= 19 || !player.dealtCards.some(x => x === cardNumber)){
+            nextCardNumber = cardNumber;
+          }
+        }
+        this.$store.commit(types.DEAL_CARD, { card:nextCardNumber});
+        var cardData = data.cards[nextCardNumber];
+        this.image = `./dist/${nextCardNumber.toString().padStart(2, '0')}.png`
+      }
+    },
   };
 </script>
